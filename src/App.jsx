22e22f9f3a4b2f0542835380bbdd657ec9dc2b82
Inga <38925063+inga-sinkeviciute@users.noreply.github.com>
2023-07-19 19:00:28 +0300

    Update App.jsx
    Adjusting React Router, so that github can publish it to its' pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import { lazy, useEffect } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageLayout from './pages/PageLayout';
 import ProtectedRoute from './shared/ProtectedRoute/ProtectedRoute';
 
 const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const TodoListPage = lazy(() => import('./pages/TodoListPage/TodoListPage'));
 
+function ScrollToTop() {
+  window.scrollTo(0, 0);
+  return null;
+}
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <PageLayout />,
-      children: [
-        {
-          path: '',
-          element: <HomePage />,
-        },
-        {
-          path: 'todos',
-          element: (
-            <ProtectedRoute>
-              <TodoListPage />
-            </ProtectedRoute>
-          ),
-        },
-      ],
-    },
-  ]);
-
-  const useScrollToTop = () => {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [pathname]);
-  };
-
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="todos" element={<ProtectedRoute><TodoListPage /></ProtectedRoute>} />
+        </Route>
+      </Routes>
+      <ScrollToTop />
+    </BrowserRouter>
+  );
 }
 
 export default App;
